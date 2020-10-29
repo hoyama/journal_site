2e481623993ed895097a022c36c544aec8ef8a1e
hoyama <ghettomoon00@gmail.com>
2020-10-29 11:08:22 +0900

    課題1修正2

--- server/resources/views/article/edit.blade.php
+++ server/resources/views/article/edit.blade.php
@@ -4,17 +4,17 @@
     <p>
         <b>論文タイトル</b>
     </p>
-    <input type="string" name="title"  value ="{{ old('title') }}";><br>
+    <input type="text" name="title"  value ="{{ old('title') }}";><br>
 
     <p>
         <b>本文</b>
     </p>
 
 
-    <input type="text" name="body"  value ="{{ old('body') }}";>
+    <textarea type="text" name="body"  value ="{{ old('body') }}";></textarea>
     <br>
 
-    <input type="submit" value="投稿" value ="{{old('')}}">
+    <input type="submit" value="投稿">
 
     {{-- <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};"> --}}
 </form>
--- server/resources/views/article/index.blade.php
+++ server/resources/views/article/index.blade.php
@@ -1,8 +1,8 @@
 <h1>論文一覧</h1>
 
 <ul>
-    @foreach ($articles as $article)
-    <li><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></li>
+    @foreach ( $articles as $article )
+    <p><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></p>
     @endforeach
 
 <a href="/articles/{{ $article->id }}/"><button>新規論文投稿</button></a>
